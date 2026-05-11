8faf50b4909d0678dcdae46e231e0569762245c8
paulosabebe05-png <paulosabebe05@gmail.com>
2026-05-11 17:19:00 +0300

    Update Footer.js

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,4 +1,5 @@
 'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import { useLang } from '../../lib/lang'
@@ -10,6 +11,8 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.inner}>
+
+        {/* Brand */}
         <div className={styles.brand}>
           <div className={styles.logo}>
             <Image
@@ -21,47 +24,95 @@ export default function Footer() {
             />
             <span>Addora</span>
           </div>
-          <p>{tr('footerTagline')}</p>
+
+          <p>{tr('footerBrandDesc')}</p>
+
           <div className={styles.codBadge}>
-            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
               <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/>
             </svg>
-            {tr('cashOnDelivery')}
+
+            {tr('footerCOD')}
           </div>
         </div>
 
+        {/* Links */}
         <div className={styles.links}>
+
+          {/* Shop */}
           <div className={styles.col}>
-            <h4>{tr('shop')}</h4>
-            <Link href="/">{tr('allProducts')}</Link>
-            <Link href="/?cat=kids">{tr('kidsClothing')}</Link>
-            <Link href="/cart">{tr('myCart')}</Link>
-            <Link href="/orders">{tr('myOrders')}</Link>
+            <h4>{tr('footerShop')}</h4>
+
+            <Link href="/">
+              {tr('footerAllProducts')}
+            </Link>
+
+            <Link href="/?cat=kids">
+              {tr('footerKidsClothing')}
+            </Link>
+
+            <Link href="/cart">
+              {tr('footerMyCart')}
+            </Link>
+
+            <Link href="/orders">
+              {tr('footerMyOrders')}
+            </Link>
           </div>
 
+          {/* Delivery */}
           <div className={styles.col}>
-            <h4>{tr('delivery')}</h4>
-            <p>{tr('addisDelivery')}</p>
-            <p>{tr('otherCitiesDelivery')}</p>
-            <p>{tr('payOnReceive')}</p>
-            <p>{tr('freeReturns')}</p>
+            <h4>{tr('footerDelivery')}</h4>
+
+            <p>{tr('footerDeliveryAddis')}</p>
+            <p>{tr('footerDeliveryCities')}</p>
+            <p>{tr('footerPayReceive')}</p>
+            <p>{tr('footerFreeReturns')}</p>
           </div>
 
+          {/* Contact */}
           <div className={styles.col}>
-            <h4>{tr('contact')}</h4>
-            <a href="tel:+251926635307">+251 926 635 307</a>
-            <a href="mailto:addora@addora.com.et">addora@addora.com.et</a>
-            <p>Addis Ababa, Ethiopia</p>
+            <h4>{tr('footerContact')}</h4>
+
+            <a href="tel:+251926635307">
+              +251 926 635 307
+            </a>
+
+            <a href="mailto:addora@addora.com.et">
+              addora@addora.com.et
+            </a>
+
+            <p>{tr('footerLocation')}</p>
           </div>
         </div>
       </div>
 
+      {/* Bottom */}
       <div className={styles.bottom}>
-        <p>© {new Date().getFullYear()} {tr('footerCopyright')}</p>
+        <p>
+          © {new Date().getFullYear()} Addora Technology PLC.{' '}
+          {tr('footerRights')}
+        </p>
+
         <div className={styles.bottomLinks}>
-          <Link href="/privacy">{tr('privacyPolicy')}</Link>
-          <Link href="/terms">{tr('termsOfService')}</Link>
-          <Link href="/refund-policy">{tr('refundPolicy')}</Link>
+          <Link href="/privacy">
+            {tr('footerPrivacy')}
+          </Link>
+
+          <Link href="/terms">
+            {tr('footerTerms')}
+          </Link>
+
+          <Link href="/refund-policy">
+            {tr('footerRefund')}
+          </Link>
         </div>
       </div>
     </footer>
